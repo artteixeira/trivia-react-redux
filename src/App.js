3fbdb3313b040a5bffc647be0ed296526a558fcf
Arthur Teixeira <arthursony1@hotmail.com>
2023-05-22 17:24:48 -0300

    requisito 6(eu acho)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import login from './pages/Login';
+import Game from './pages/Game';
 
 export default function App() {
   return (
     <main>
       <Switch>
         <Route exact path="/" component={ login } />
+        <Route exact path="/game" component={ Game } />
       </Switch>
     </main>
   );
